9f2a3e911f4eb81f9379bc25f1b471035582ba41
ArekStasko <arek.stasko@gmail.com>
2021-09-17 03:38:33 -0700

    add console log to debug registration v3 SUCCESS

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`SERVER IS LISTENING ON PORT ${port}`);
--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -4,13 +4,6 @@ const passport = require("passport");
 module.exports.userRegistration = async (req, res, next) => {
   try {
     const { email, username, place, phonenumber, password } = req.body;
-    console.log(req.body.data, {
-      email, 
-      username, 
-      place, 
-      phonenumber, 
-      password
-    })
     const user = new User({ email, username, place, phonenumber });
     user.image = req.file.path
     const registeredUser = await User.register(user, password);
