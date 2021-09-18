b395744ab27a671f624865278ea82cf1cac3439f
ArekStasko <arek.stasko@gmail.com>
2021-09-18 15:45:54 -0700

    add isLoggedIn

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,6 +1,15 @@
 const Product = require('../models/product')
 const User = require("../models/user");
 
+module.exports.isLoggedIn = async(req, res, next) => {
+    const { id } = req.params
+    const user = await User.findById(id)
+    if(!user){
+        return res.send('you must be logged in')
+    }
+    next()
+}
+
 module.exports.isAuthor = async(req, res, next) => {
   const { id, userID } = req.params
   const product = await Product.findById(id)
